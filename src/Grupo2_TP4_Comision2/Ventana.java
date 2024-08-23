0a32b6fbe2d750d92b307a905accc6a86a0f3941
AlecFrame <walteravertacnik@sanluis.edu.ar>
2024-08-23 12:11:59 -0300

    Initial commit

---
+++ src/Grupo2_TP4_Comision2/Ventana.java
@@ -0,0 +1,51 @@
+
+package Grupo2_TP4_Comision2;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+
+
+public class Ventana extends javax.swing.JFrame {
+    
+    /// Participantes - Grupo 2 - Comision 2 - TP4 ///
+    // > Walter Alexander Vertacnik - DNI 46260391
+    // > Soto Vela Luciano Ezequiel - DNI
+    // > Ferro Julieta - DNI
+    // > Pagnone Patricia - DNI
+    // > Muñoz Maycol - DNI
+    
+    @SuppressWarnings("unchecked") // NO modificar el Generated Code de aqui abajo
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 300, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    public static void main(String args[]) {
+        
+        /////////////////// Ventana Propiedades /////////////////////////////////
+        JFrame ventana = new JFrame("NombreVentana");
+        ventana.setBackground(Color.white);
+        ventana.setSize(400,400); //Valores por defecto por ahora
+        ventana.setVisible(true);
+        /////////////////////////////////////////////////////////////////////////
+        
+        
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+}
