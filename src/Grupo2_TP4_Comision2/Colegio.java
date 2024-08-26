8ac3ff347fb31fafec7fe3a13e983a8ecc7538da
AlecFrame <walteravertacnik@sanluis.edu.ar>
2024-08-26 17:50:55 -0300

    Trabajo en Equipo
    subo todo lo que vimos mientras estabamos compartiendo

--- src/Grupo2_TP4_Comision2/Colegio.java
+++ src/Grupo2_TP4_Comision2/Colegio.java
@@ -1,36 +1,37 @@
 
 package Grupo2_TP4_Comision2;
 
-import Grupo2_TP4.Vistas.Principal;
-
-
-
+import Vistas.Principal;
+import Modelos.Alumno;
+import Modelos.Materia;
 
 public class Colegio {
 
-    
     public static void main(String[] args) {
         
-        /*Materia m1= new Materia(1, 1,"Ingles 1");
-        Materia m2= new Materia(2,1,"Matematicas");
-        Materia m3= new Materia(3,1, "Laboratorio");
+        Materia m1 = new Materia(1, 1,"Ingles 1");
+        Materia m2 = new Materia(2,1,"Matematicas");
+        Materia m3 = new Materia(3,1, "Laboratorio");
         
         Alumno a1= new Alumno(1001,"Lopez", "Martin");
         Alumno a2= new Alumno(1002, "Martinez", "Brenda");
         
+        
         a1.agregarMateria(m1);
         a1.agregarMateria(m3);
         a1.agregarMateria(m2);
+        
         a2.agregarMateria(m3);
         a2.agregarMateria(m2);
         a2.agregarMateria(m1);
         a2.agregarMateria(m3);
         
-        System.out.println(a1.cantidadDeMaterias());
-        System.out.println(a2.cantidadDeMaterias());
-        */
         Principal principal= new Principal();
         principal.setVisible(true);
+        
+        System.out.println(a1.getApellido()+" "+a1.getNombre()+" Se ha inscrito en "+a1.cantidadDeMaterias()+" materias");
+        System.out.println(a2.getApellido()+" "+a2.getNombre()+" Se ha inscrito en "+a2.cantidadDeMaterias()+" materias");
+        
         
        
         
